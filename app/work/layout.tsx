36185893f86b1a904f789e276a86968df55d785a
Aaron Wright <aaronmw@gmail.com>
2026-07-31 18:02:28 -0600

    Refine portfolio link styling

--- app/work/layout.tsx
+++ app/work/layout.tsx
@@ -3,7 +3,7 @@ import { Open_Sans } from 'next/font/google';
 
 const openSans = Open_Sans({
   subsets: ['latin'],
-  weight: ['300', '800'],
+  weight: ['300', '500', '800'],
 });
 
 export const metadata: Metadata = {
